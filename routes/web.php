5fef6fcf57e5912bc690de19de796e91158568d5
iqbalmudzakky <iqbalmudzakky@gmail.com>
2026-01-11 17:41:43 +0700

    feat: update web routes to include greeting and fallback functionality

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+  return "Welcome to the Home Page!";
+});
+
+Route::get('/xxx', function() {
+  return 'Hello from the other side';
+}) -> name('hello');
+
+Route::get('/hallo', function() {
+  return redirect() -> route('hello');
+});
+
+Route::get('/greet/{name}', function ($name) {
+  return 'Hello, ' . $name . '!';
+});
+
+Route::fallback(function () {
+  return 'Nothing in here!';
 });
