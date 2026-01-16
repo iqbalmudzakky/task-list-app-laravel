bfca5d22df60cf0b8b7a1bb183828466d3fb6b16
iqbalmudzakky <iqbalmudzakky@gmail.com>
2026-01-16 18:32:01 +0700

    feat: create Task model, factory, migration, and update DatabaseSeeder for task seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +16,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+      // Seed users
+      User::factory(10)->create();
+      User::factory(2)->unverified()->create(); // Create 2 custom method (unverified) users
+      
+      // Seed tasks
+      Task::factory(20)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+      // User::factory()->create([
+      //     'name' => 'Test User',
+      //     'email' => 'test@example.com',
+      // ]);
     }
 }
